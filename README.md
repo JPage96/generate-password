53731155c7e98cf8b30bafaf3f900d67d2f0eb6b
JPage96 <justice.page1@pepsico.com>
2022-04-20 20:27:19 -0500

    added password questions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Discription
+
 AS AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
 SO THAT I can create a strong password that provides greater security
@@ -17,4 +19,6 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 
+
+# Screenshot
 ![mockup](./assets/images/mockup.png "mockup")
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,53 @@
 // Assignment code here
+//characters to be included in password
+var specialCharacters = [
+  '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
+];
+
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var lowerCasedCharacters = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+var upperCasedCharacters = [
+  'A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+
 function generatePassword() {
+
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
+
+  if (isNaN(length) === true) {
+    alert('Password length must be provided as a number');
+    return
+  }
+
+  if (length < 8) {
+    alert('Password length must be at least 8 characters');
+    return;
+  }
+
+  if (length > 128) {
+    alert('Password length must less than 129 characters');
+    return;
+  }
+
+  var hasSpecialCharacters = confirm(
+    'Click OK to confirm including special characters.'
+  );
+
+  var hasLowerCasedCharacters = confirm(
+    'Click OK to confirm including lowercase characters.'
+  );
+
+  var hasUpperCasedCharacters = confirm(
+    'Click OK to confirm including uppercase characters.'
+  );
+
+
 }
 
 // Get references to the #generate element
